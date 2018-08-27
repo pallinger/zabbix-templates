3677f88a8a53cdfa48822569b80863d3139a3b4b
Pallinger Péter <pallinger@dsd.sztaki.hu>
2018-08-27 10:04:34 +0200

    discover nvme and md devices for iostat

--- iostat/dev-discovery.sh
+++ iostat/dev-discovery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEVICES=`iostat | awk '{ if ($1 ~ "^([shxv]|xv)d[a-z]$") { print $1 } }'`
+DEVICES=`iostat | awk '{ if ($1 ~ "^([shxv]|xv)d[a-z]$" || $1 ~ "^md[0-9]$" || $1 ~ "^nvme[0-9]n[0-9]$" ) { print $1 } }'`
 
 COUNT=`echo "$DEVICES" | wc -l`
 INDEX=0
